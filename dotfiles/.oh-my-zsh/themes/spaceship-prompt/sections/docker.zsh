72fe08ff9c6bb2b943481ce24d94f6c65a6dd096
Ricardo Madriz <rmadriz@keplergrp.com>
2018-09-20 12:44:27 -0600

    Move spaceship theme to correct destination

---
+++ dotfiles/.oh-my-zsh/themes/spaceship-prompt/sections/docker.zsh
@@ -0,0 +1,46 @@
+#
+# Docker
+#
+# Docker automates the repetitive tasks of setting up development environments
+# Link: https://www.docker.com
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+SPACESHIP_DOCKER_SHOW="${SPACESHIP_DOCKER_SHOW=true}"
+SPACESHIP_DOCKER_PREFIX="${SPACESHIP_DOCKER_PREFIX="on "}"
+SPACESHIP_DOCKER_SUFFIX="${SPACESHIP_DOCKER_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_DOCKER_SYMBOL="${SPACESHIP_DOCKER_SYMBOL="🐳 "}"
+SPACESHIP_DOCKER_COLOR="${SPACESHIP_DOCKER_COLOR="cyan"}"
+SPACESHIP_DOCKER_VERBOSE="${SPACESHIP_DOCKER_VERBOSE=false}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+# Show current Docker version and connected machine
+spaceship_docker() {
+  [[ $SPACESHIP_DOCKER_SHOW == false ]] && return
+
+  spaceship::exists docker || return
+
+  # Show Docker status only for Docker-specific folders
+  [[ -f $COMPOSE_FILE || -f Dockerfile || -f docker-compose.yml ]] || return
+
+  # if docker daemon isn't running you'll get an error saying it can't connect
+  local docker_version=$(docker version -f "{{.Server.Version}}" 2>/dev/null)
+  [[ -z $docker_version ]] && return
+
+  [[ $SPACESHIP_DOCKER_VERBOSE == false ]] && docker_version=$(echo ${docker_version} | awk -F\- '{ print $1 }')
+
+  if [[ -n $DOCKER_MACHINE_NAME ]]; then
+    docker_version+=" via ($DOCKER_MACHINE_NAME)"
+  fi
+
+  spaceship::section \
+    "$SPACESHIP_DOCKER_COLOR" \
+    "$SPACESHIP_DOCKER_PREFIX" \
+    "${SPACESHIP_DOCKER_SYMBOL}v${docker_version}" \
+    "$SPACESHIP_DOCKER_SUFFIX"
+}
