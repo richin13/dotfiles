daf8c1078f2e8a3001982817fe43ef8eec8ac0cf
Ricardo Madriz <rmadriz@keplergrp.com>
2022-06-23 15:25:24 -0600

    Change telescope layout

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,12 @@
 -- Telescope.nvim configuration
 local ts = require('telescope')
-local actions = require('telescope.actions')
 
 ts.setup({
   defaults = {
     prompt_prefix = " ",
+    layout_strategy = "vertical",
+    layout_config = {
+      vertical = { width = 0.90 },
+    },
   },
 })
