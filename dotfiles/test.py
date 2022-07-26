aad2afcbe33416559885615b5e45328cce0fcd30
Ricardo Madriz <richin13@gmail.com>
2022-07-25 21:48:21 -0600

    Update test script

--- dotfiles/test.py
+++ dotfiles/test.py
@@ -1,42 +1,32 @@
 """Some docstring."""
+from typing import Literal
+
+MY_CONST: Literal["111"] = "111"
 
 
-d = {
-        'h': 1
-        }
+class M:
+    ...
 
+    def __init__(self):
+        #: TODO: do something useful
+        #: FIXME: fix something useful
+        #: XXX: urgent
+        print(self)
+        self.cal = f"{self.b}"
+
+
+@required
 def main():
     """Main entrypoint."""
-    for i in range():
+    for iiiiiii in range(1):
         ...
     print("hello, world")
+    return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 
-
-
-
-
-
-
-
-
 val = 1
-val + "1"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+val += "1"
