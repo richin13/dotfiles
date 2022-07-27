55dce44d6f77c82881b074e750b758e4b1f3723e
Ricardo Madriz <richin13@gmail.com>
2022-07-27 17:32:51 -0600

    Fix lint issue

--- dotfiles/.config/tmux/scripts/dst.sh
+++ dotfiles/.config/tmux/scripts/dst.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function dst {
-  local caldate=$(date +%m.%d)
-  local cr_time=$(TZ=America/Costa_Rica date +%H)
-  local ny_time=$(TZ=US/Eastern date +%H);
-  local minute=$(date +%M)
+  local caldate, cr_time, ny_time, minute
+  caldate=$(date +%m.%d)
+  cr_time=$(TZ=America/Costa_Rica date +%H)
+  ny_time=$(TZ=US/Eastern date +%H);
+  minute=$(date +%M)
 
   local cmp_et="#[fg=#bd93f9,bold]$ny_time#[fg=#f8f8f2,none]"
   echo "#[fg=#f8f8f2]$caldate $cr_time($cmp_et):$minute"
