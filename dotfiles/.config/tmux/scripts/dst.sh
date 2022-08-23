2f7f1a3fb4829ba0f01066d2005297858767385f
Ricardo Madriz <richin13@gmail.com>
2022-08-23 07:42:47 -0600

    Show hour diff in dst script

--- dotfiles/.config/tmux/scripts/dst.sh
+++ dotfiles/.config/tmux/scripts/dst.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function dst {
-  local caldate, cr_time, ny_time, minute
+  local caldate, cr_time, ny_time_diff, minute
   caldate=$(date +%m.%d)
   cr_time=$(TZ=America/Costa_Rica date +%H)
-  ny_time=$(TZ=US/Eastern date +%H);
+  ny_time_diff=$((10#$(TZ=US/Eastern date +%H) - 10#$(date +%H)))
   minute=$(date +%M)
 
-  local cmp_et="#[fg=#bd93f9,bold]$ny_time#[fg=#f8f8f2,none]"
-  echo "#[fg=#f8f8f2]$caldate $cr_time($cmp_et):$minute"
+  local cmp_et="#[fg=#bd93f9,bold]+$ny_time_diff#[fg=#f8f8f2,none]"
+  echo "#[fg=#f8f8f2]$caldate $cr_time$cmp_et:$minute"
 }
 
 dst
