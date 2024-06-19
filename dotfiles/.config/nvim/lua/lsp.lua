875f05039eaa7414d096fc10d36f74cdb0b14c1b
Ricardo Madriz <richin13@gmail.com>
2024-06-19 15:08:19 -0600

    Fix issue with diagnostics and conflict with hover

--- dotfiles/.config/nvim/lua/lsp.lua
+++ dotfiles/.config/nvim/lua/lsp.lua
@@ -1,6 +1,67 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+local on_attach = function(client, bufnr)
+  if client and (client.name == "dockerls" or client.name == "tsserver") then
+    -- Disable semantic tokens for dockerls and tsserver
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      callback = function()
+        vim.lsp.buf.document_highlight()
+      end,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      callback = function()
+        vim.lsp.buf.clear_references()
+      end,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+    })
+  end
+
+  local _border = "rounded"
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = _border,
+  })
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = _border,
+  })
+  vim.diagnostic.config({
+    virtual_text = false,
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.INFO] = "",
+        [vim.diagnostic.severity.HINT] = "",
+      },
+    },
+    float = {
+      border = _border,
+      header = "",
+      prefix = "",
+    },
+  })
+
+  -- Keymappings for LSP
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set("n", "<leader>gy", vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<space>d", function()
+    vim.diagnostic.open_float(bufnr, { scope = "cursor" })
+  end, opts)
+end
+
 -- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local configs = {
   -- https://github.com/bash-lsp/bash-language-server
@@ -50,72 +111,7 @@ local configs = {
 
 for server, config in pairs(configs) do
   lspconfig[server].setup(vim.tbl_deep_extend("force", {
+    on_attach = on_attach,
     capabilities = capabilities,
   }, config))
 end
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-    if client and (client.name == "dockerls" or client.name == "tsserver") then
-      -- Disable semantic tokens for dockerls and tsserver
-      client.server_capabilities.semanticTokensProvider = nil
-    end
-
-    if client and client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        pattern = "*",
-        callback = function()
-          vim.lsp.buf.document_highlight()
-          -- Show line diagnostics automatically in hover window
-          vim.diagnostic.open_float(nil, { focus = false })
-        end,
-      })
-      vim.api.nvim_create_autocmd("CursorMoved", {
-        pattern = "*",
-        callback = function()
-          vim.lsp.buf.clear_references()
-        end,
-      })
-    end
-  end,
-})
-local _border = "rounded"
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = _border,
-})
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = _border,
-})
-vim.diagnostic.config({
-  virtual_text = false,
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "",
-      [vim.diagnostic.severity.WARN] = "",
-      [vim.diagnostic.severity.INFO] = "",
-      [vim.diagnostic.severity.HINT] = "",
-    },
-  },
-  float = {
-    border = _border,
-  },
-})
-
--- Keymappings for LSP
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gy",
-  "<cmd>lua vim.lsp.buf.type_definition()<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gi",
-  "<cmd>lua vim.lsp.buf.implementation()<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
