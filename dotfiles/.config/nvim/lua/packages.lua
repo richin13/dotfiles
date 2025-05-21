4aec088c4c1cead060ac1b3ae5d430ebd3ff4a63
Ricardo Madriz <richin13@gmail.com>
2025-05-20 20:58:58 -0600

    Disable fancy msg box

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -295,12 +295,12 @@ require("telescope").setup({
   },
 })
 -- }}}
-require("vim._extui").enable({
-  enable = true,
-  msg = {
-    pos = "box",
-    box = {
-      timeout = 4000,
-    },
-  },
-})
+-- require("vim._extui").enable({
+--   enable = true,
+--   msg = {
+--     pos = "box",
+--     box = {
+--       timeout = 4000,
+--     },
+--   },
+-- })
