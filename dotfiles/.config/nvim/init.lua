240992d6820cb3bb276dbfce19de08715af53d92
Ricardo Madriz <richin13@gmail.com>
2026-05-11 07:01:30 -0600

    Render tab as 2 spaces

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -94,7 +94,7 @@ vim.api.nvim_create_autocmd("FileType", {
       vim.opt_local.expandtab = true
       vim.opt_local.shiftwidth = 2
       vim.opt_local.softtabstop = 2
-      vim.opt_local.tabstop = 8
+      vim.opt_local.tabstop = 2
     end
   end,
 })
